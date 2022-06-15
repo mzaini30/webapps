8db2f5ec4a53ca739395e3aa90a82eba095acb42
Zen <muhzaini30@gmail.com>
2022-06-16 05:36:21 +0800

    chore: add funding

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
 import routes from "virtual:generated-pages";
-import prefetch from 'vue-router-prefetch'
+import prefetch from "vue-router-prefetch";
 
 export const createApp = ViteSSG(
   App,
   { routes },
   ({ app, router, initialState }) => {
-  	app.use(prefetch)
+    app.use(prefetch);
   }
 );
