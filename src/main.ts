c002cb180d277ec51f551b0e59b5c10c13c94913
Zen <muhzaini30@gmail.com>
2022-06-16 13:17:59 +0800

    chore: experiment with build script

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,32 @@
-import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
+import { createApp } from "vue";
 import routes from "virtual:generated-pages";
 import "@unocss/reset/tailwind.css";
 import "uno.css";
 import VueLazyload from "vue-lazyload";
+import { createRouter, createWebHistory } from "vue-router";
+import {createHead} from '@vueuse/head'
+import RouterPrefetch from 'vue-router-prefetch'
 
-export const createApp = ViteSSG(
-  App,
-  { routes },
-  ({ app}) => {
-    app.use(VueLazyload);
-  }
-);
+const router = createRouter({
+  routes,
+  history: createWebHistory(),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
+})
+
+const head = createHead()
+
+const app = createApp(App)
+
+app
+.use(router)
+.use(RouterPrefetch)
+.use(head)
+.use(VueLazyload)
+.mount('#app')
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,17 @@
+<script setup="">
+  import {Head} from '@vueuse/head'
+</script>
+
 <template>
-  <p>hello world</p>
+  <Head>
+    <title>Excalibur</title>  
+  </Head>
+
+  <div class="text-center p-10 min-h-screen w-full grid items-center">
+    <div class="-mt-20">
+      <h1 class="text-3xl mb-1">Excalibur</h1>
+      <p class="text-lg mb-1">All apps inside your browser</p>
+      <p class="italic">Coming soon</p>
+    </div>
+  </div>
 </template>
