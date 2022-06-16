61c88242cb1926ebde8ce677af555092ee661c2f
Zen <muhzaini30@gmail.com>
2022-06-16 11:21:47 +0800

    chore: init host

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,14 @@ import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
 import routes from "virtual:generated-pages";
 import prefetch from "vue-router-prefetch";
-import '@unocss/reset/tailwind.css'
-import 'uno.css'
+import "@unocss/reset/tailwind.css";
+import "uno.css";
 import VueLazyload from "vue-lazyload";
 
 export const createApp = ViteSSG(
   App,
   { routes },
   ({ app, router, initialState }) => {
-    app.use(prefetch)
-    	 .use(VueLazyload)
+    app.use(prefetch).use(VueLazyload);
   }
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
-import Components from 'unplugin-vue-components/vite'
-import inspector from 'vite-plugin-vue-inspector'
-import unocss from 'unocss/vite'
-import yaml from 'vite-plugin-yaml2'
-import apply from '@unocss/transformer-directives'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Pages from "vite-plugin-pages";
+import Components from "unplugin-vue-components/vite";
+import inspector from "vite-plugin-vue-inspector";
+import unocss from "unocss/vite";
+import yaml from "vite-plugin-yaml2";
+import apply from "@unocss/transformer-directives";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,18 +13,16 @@ export default defineConfig({
     vue(),
     yaml(),
     unocss({
-      transformers: [
-        apply()
-      ]
+      transformers: [apply()],
     }),
     Components({
-      extensions: ['vue', 'md'],
+      extensions: ["vue", "md"],
       // allow auto import and register components used in markdown
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
     }),
-      Pages({
-      extensions: ['vue', 'md'],
+    Pages({
+      extensions: ["vue", "md"],
     }),
-    inspector()
-  ]
-})
+    inspector(),
+  ],
+});
